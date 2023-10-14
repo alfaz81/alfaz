a0f2bbb8a5b115a14aba611284cd7668556af5f4
alsa <kaltex.alsa@gmail.com>
2023-10-14 20:24:16 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
 
 function App() {
-  const [cart, setCart] = useState([]);
-  const [products] = useState([
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 30 },
-  ]);
-
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
-  const removeFromCart = (productId) => {
-    const updatedCart = cart.filter((item) => item.id !== productId);
-    setCart(updatedCart);
-  };
-
-  const cartTotal = cart.reduce((total, product) => total + product.price, 0);
-
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <div className="product-list">
-        <h2>Products:</h2>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              {product.name} - ${product.price}
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="cart">
-        <h2>Cart:</h2>
-        <ul>
-          {cart.map((product) => (
-            <li key={product.id}>
-              {product.name} - ${product.price}
-              <button onClick={() => removeFromCart(product.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-        <p>Total: ${cartTotal}</p>
-      </div>
+    <div class="container">
+
+
+    <h1>Hello welcome to my website its still developing //
+       developed  by Alfaz kaladagi</h1>
     </div>
-  );
+  )
 }
 
-export default App;
- 
-
- 
+export default App
